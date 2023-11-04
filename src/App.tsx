78aa3df1ef687d3ed7f0a508368c95fde0341bad
Jay Ahn <joosung21@gmail.com>
2023-11-05 08:57:34 +0900

    mui 설치완료

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import AppRouter from './router/AppRouter'
 import './App.scss'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 const App = () => {
   return (
     <div className="App">
--- src/pages/Mui.tsx
+++ src/pages/Mui.tsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import Button from '@mui/material/Button'
 
 const Mui = () => {
   return (
-    <div>
+    <div className="limit-width">
       <div className="page-title">Mui</div>
+
+      <p>설치</p>
+      <div className="code">npm install @mui/material @emotion/react @emotion/styled</div>
+
+      <div className="code">npm install @fontsource/roboto</div>
+
+      <p>App.tsx에 import</p>
+      <div className="code">
+        <pre>
+          {`import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'`}
+        </pre>
+      </div>
+
+      <p>사용</p>
+      <div className="code">
+        <pre>
+          {`import Button from '@mui/material/Button'
+...
+<Button variant="contained">Hello World</Button>`}
+        </pre>
+      </div>
+
+      <Button variant="contained">Hello World</Button>
+
+      <br />
+
+      <div className="mt-4">MUI 문서</div>
+      <a
+        href="https://mui.com/material-ui/getting-started/installation/"
+        target="_blank"
+        rel="noreferrer"
+        className="text-link"
+      >
+        MUI
+      </a>
+
+      <br />
+
+      <div className="mt-4">Tutorial</div>
+      <a
+        href="https://www.youtube.com/playlist?list=PLDxCaNaYIuUlG5ZqoQzFE27CUOoQvOqnQ"
+        target="_blank"
+        rel="noreferrer"
+        className="text-link"
+      >
+        React + Material UI - From zero to hero
+      </a>
     </div>
   )
 }
