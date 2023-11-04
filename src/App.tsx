3ade48dddb4389f0e31d780282ce17a1905de8e6
Jay Ahn <joosung21@gmail.com>
2023-11-04 21:41:32 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import './App.scss';
+import AppRouter from "./router/AppRouter";
+import "./App.scss";
 
 // Components
-import Left from './layout/Left';
-import NotFound from './pages/NotFound';
-import Todo from './pages/Todo';
-import Setting1 from './pages/Setting1';
-import Setting2 from './pages/Setting2';
-import Setting3 from './pages/Setting3';
-import Hook1 from './pages/Hook1';
 
 const App = () => {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Left />
-        <div className='main'>
-          <Routes>
-            <Route path="/" element={<Navigate to="/setting1" />} />
-            <Route path="/setting1" element={<Setting1 />} />
-            <Route path="/setting2" element={<Setting2 />} />
-            <Route path="/setting3" element={<Setting3 />} />
-            <Route path="/hook1" element={<Hook1 />} />
-            <Route path="/todo" element={<Todo />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+    <div className="App">
+      <AppRouter />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Left from "../layout/Left";
+import NotFound from "../pages/NotFound";
+import Todo from "../pages/Todo";
+import Setting1 from "../pages/Setting1";
+import Setting2 from "../pages/Setting2";
+import Setting3 from "../pages/Setting3";
+import Hook1 from "../pages/Hook1";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Left />
+
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Navigate to="/setting1" />} />
+          <Route path="/setting1" element={<Setting1 />} />
+          <Route path="/setting2" element={<Setting2 />} />
+          <Route path="/setting3" element={<Setting3 />} />
+          <Route path="/hook1" element={<Hook1 />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
