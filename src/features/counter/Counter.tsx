5d5863cd9289ad51feaf70c5492699164cf0de04
Jay Ahn <joosung21@gmail.com>
2023-11-05 16:04:57 +0900

    wip

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -9,8 +9,8 @@ import {
   incrementIfOdd,
   selectCount,
 } from './counterSlice'
+import { Card, CardContent, IconButton, CardHeader } from '@mui/material'
 import styles from './Counter.module.css'
-import IconButton from '@mui/material/IconButton'
 import AddBoxIcon from '@mui/icons-material/AddBox'
 import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox'
 
@@ -24,7 +24,7 @@ export function Counter() {
   return (
     <div className="mt-16">
       <div className="text-sm text-gray-500">useAppSelector를 이용해 작동하는 count state 값</div>
-      <div className="flex space-x-2 items-center">
+      <div className="flex space-x-2 items-center my-5">
         <IconButton
           aria-label="delete"
           color="primary"
@@ -46,46 +46,54 @@ export function Counter() {
         </IconButton>
       </div>
 
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button className={styles.button} onClick={() => dispatch(incrementIfOdd(incrementValue))}>
-          Add If Odd
-        </button>
-      </div>
+      <Card sx={{ minWidth: 600, maxWidth: 800 }}>
+        <CardHeader title="Counter Component" />
+        <CardContent>
+          <div className={styles.row}>
+            <button
+              className={styles.button}
+              aria-label="Decrement value"
+              onClick={() => dispatch(decrement())}
+            >
+              -
+            </button>
+            <span className={styles.value}>{count}</span>
+            <button
+              className={styles.button}
+              aria-label="Increment value"
+              onClick={() => dispatch(increment())}
+            >
+              +
+            </button>
+          </div>
+          <div className={styles.row}>
+            <input
+              className={styles.textbox}
+              aria-label="Set increment amount"
+              value={incrementAmount}
+              onChange={e => setIncrementAmount(e.target.value)}
+            />
+            <button
+              className={styles.button}
+              onClick={() => dispatch(incrementByAmount(incrementValue))}
+            >
+              Add Amount
+            </button>
+            <button
+              className={styles.asyncButton}
+              onClick={() => dispatch(incrementAsync(incrementValue))}
+            >
+              Add Async
+            </button>
+            <button
+              className={styles.button}
+              onClick={() => dispatch(incrementIfOdd(incrementValue))}
+            >
+              Add If Odd
+            </button>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
