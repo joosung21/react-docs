46def93d65caad2b6bd3e25d32ca326ae3c9a894
Jay Ahn <joosung21@gmail.com>
2023-11-06 22:11:36 +0900

    wip

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -4,16 +4,18 @@ const Todo = () => {
   return (
     <div className="todoList">
       <ul style={{ listStyle: 'square' }}>
+        <li>UI Sandbox 디버깅</li>
+        <li>전역 store 설정, 스낵바 생성</li>
+        <li>클립보드 생성 스낵바로 변경</li>
         <li>프로필 페이지</li>
         <li>라우팅 파라미터</li>
-        <li>리액트 공식문서 1독</li>
+        <li>리액트 공식문서 마스터</li>
         <li>리액트 DevTool</li>
         <li>폼 Validation</li>
 
         <hr className="my-2" />
 
         <li>디자인패턴</li>
-        <li>클립보드 복사기능</li>
         <li>DateControl</li>
         <li>주소검색</li>
         {/* 공부할 것 */}
